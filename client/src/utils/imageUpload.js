766bc506cac4268dabd4733db9910d779391f376
Gustavo Scafeli <g.cflsouza@gmail.com>
2023-03-09 19:37:33 -0300

    mongodb and cloudinary update

--- client/src/utils/imageUpload.js
+++ client/src/utils/imageUpload.js
@@ -1,38 +1,39 @@
 export const checkImage = (file) => {
-    let err = ""
-    if(!file) return err = "File does not exist."
+  let err = "";
+  if (!file) return (err = "File does not exist.");
 
-    if(file.size > 1024 * 1024) // 1mb
-    err = "The largest image size is 1mb."
+  if (file.size > 1024 * 1024)
+    // 1mb
+    err = "The largest image size is 1mb.";
 
-    if(file.type !== 'image/jpeg' && file.type !== 'image/png' )
-    err = "Image format is incorrect."
-    
-    return err;
-}
+  if (file.type !== "image/jpeg" && file.type !== "image/png")
+    err = "Image format is incorrect.";
 
+  return err;
+};
 
 export const imageUpload = async (images) => {
-    let imgArr = [];
-    for(const item of images){
-        const formData = new FormData()
+  let imgArr = [];
+  for (const item of images) {
+    const formData = new FormData();
 
-        if(item.camera){
-            formData.append("file", item.camera)
-        }else{
-            formData.append("file", item)
-        }
-        
-        formData.append("upload_preset", "efxjficn")
-        formData.append("cloud_name", "devat-channel")
-
-        const res = await fetch("https://api.cloudinary.com/v1_1/devat-channel/upload", {
-            method: "POST",
-            body: formData
-        })
-        
-        const data = await res.json()
-        imgArr.push({public_id: data.public_id, url: data.secure_url})
+    if (item.camera) {
+      formData.append("file", item.camera);
+    } else {
+      formData.append("file", item);
     }
-    return imgArr;
-}
+
+    formData.append("upload_preset", "r9wa5nzm");
+    formData.append("cloud_name", "scafeli");
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/scafeli/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    imgArr.push({ public_id: data.public_id, url: data.secure_url });
+  }
+  console.log(imgArr);
+  return imgArr;
+};
